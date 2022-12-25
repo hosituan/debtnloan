2fc74f3a1b873825487589840833f9431b5d2a61
hosituan <hosituan.work@gmail.com>
2022-12-25 12:24:56 +0700

    Initial Commit

---
+++ Money/MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyApp.swift
+//  Money
+//
+//  Created by Tuan Si Ho (3406) on 25/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
